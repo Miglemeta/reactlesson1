e290b2d74738d4165a3e8cd00be2d96cbbadf388
Migle Zalubiene <74547959+Miglemeta@users.noreply.github.com>
2021-03-09 22:47:44 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>This is my favourite color list</h1>
+    <ul>
+      <li>Red </li>
+      <li>Blue </li>
+      <li>Green </li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
